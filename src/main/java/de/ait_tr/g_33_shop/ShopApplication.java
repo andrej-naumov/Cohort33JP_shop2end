e9b08e11d7fa2d1096b2ffa7c7cde3888f8e6ce6
Andrey <k-udm@ya.ru>
2024-06-03 22:07:53 +0200

    Initial commit

---
+++ src/main/java/de/ait_tr/g_33_shop/ShopApplication.java
@@ -0,0 +1,12 @@
+package de.ait_tr.g_33_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopApplication.class, args);
+	}
+}
