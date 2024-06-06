1210872897245a856ed0f78c4e7340ad5bfdfe21
Andrey <k-udm@ya.ru>
2024-06-06 18:17:51 +0200

    lesson 8

---
+++ src/main/java/de/ait_tr/g_33_shop/service/ProductServiceImpl.java
@@ -0,0 +1,62 @@
+package de.ait_tr.g_33_shop.service;
+
+import de.ait_tr.g_33_shop.domain.entity.Product;
+import de.ait_tr.g_33_shop.service.interfaces.ProductService;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Override
+    public Product save(Product product) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getAllActiveProducts() {
+        return null;
+    }
+
+    @Override
+    public Product getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Product update(Product product) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+
+    @Override
+    public void deleteByTitle(String title) {
+
+    }
+
+    @Override
+    public void restoreById(Long id) {
+
+    }
+
+    @Override
+    public long getAllActiveProductsQuantity() {
+        return 0;
+    }
+
+    @Override
+    public BigDecimal getAllActiveProductsTotalPrice() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getAllActiveProductsAveragePrice() {
+        return null;
+    }
+}
